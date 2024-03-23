6660f3836b07657a0fd8eaeb84f14d6cafaa43c5
rushikas569628 <reddyreducation@gmail.com>
2024-03-23 13:04:26 -0500

    pick

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hatRouter = require('./routes/hat');
 var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hat', hatRouter);
 app.use('/grid', gridRouter);
+app.use('/pick', pickRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
