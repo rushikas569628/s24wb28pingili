05ac2c3ba493b3c4a0e8d9aca02ed53d15593427
rushikas569628 <reddyreducation@gmail.com>
2024-03-23 12:30:15 -0500

    grid

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hatRouter = require('./routes/hat');
+var gridRouter = require('./routes/grid');
 var app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hat', hatRouter);
+app.use('/grid', gridRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
